b58f830ec824524af4b708f63c6c028f60df4b08
Sarabjeet <infosarabjeetkaursidhu@gmail.com>
2021-02-02 20:48:31 -0800

    hjj

---
+++ iosproject/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iosproject
+//
+//  Created by user186818 on 1/26/21.
+//  Copyright © 2021 user186818. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    
+    }overridefunction
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
